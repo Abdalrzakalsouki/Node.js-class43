175743e49d1b92426e6ac311405f8a342c072589
Abdulrazzak Alsssouki <abdulrzakalsssouki@gmail.com>
2023-06-04 23:26:47 +0200

    add env file, minor change for hanlding response

--- homework/hackyourtemperature/app.js
+++ homework/hackyourtemperature/app.js
@@ -1,9 +1,9 @@
 import express from "express";
 import fetch from "node-fetch";
-import keys from "./sources/keys.js";
+import dotenv from "dotenv";
 
 const app = express();
-
+dotenv.config();
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -11,29 +11,30 @@ app.get("/", (req, res) => {
 });
 
 app.post("/weather", async (req, res, next) => {
-  const key = keys.API_KEY;
+  const key = process.env.API_KEY;
   const cityName = req.body.cityName;
   const url = `https://api.openweathermap.org/data/2.5/weather`;
   const fetchOptions = {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   };
-  let weatherText;
   try {
     const fetchResponse = await fetch(
       `${url}?q=${cityName}&APPID=${key}`,
       fetchOptions
     );
     const result = await fetchResponse.json();
-    if (result.cod === 200)
-      weatherText = `City: ${cityName}, Temperature: ${result.main.temp}`;
-    else weatherText = "City is not found!";
+    if (result.cod === 200) {
+      const weatherText = `City: ${cityName}, Temperature: ${result.main.temp}`;
+      res.send(weatherText);
+    } else {
+      res.send("City is not found!");
+    }
   } catch (error) {
     console.error(`Something wrong happend: ${error}`);
     res.status(500);
     next(error);
   }
-  res.send(weatherText);
 });
 
 export default app;
--- homework/hackyourtemperature/sources/keys.js
+++
@@ -1,5 +0,0 @@
-const keys = {
-  API_KEY: "e10b4a73dbec03717c53ecf16435c73c",
-};
-
-export default keys;
